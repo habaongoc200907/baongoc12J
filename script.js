05d3099bd04e2efe56bcb503d221e06422c2d680
habaongoc200907 <hbaongoc200907@gmail.com>
2025-02-21 14:13:33 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u=username.value
+  let p=pw.value
+  if(u=="bngoc" && p=="200907")
+  {
+    window.open("home.html")
+    document.getElementById(compiler).style.display="block"
+  }
+}
